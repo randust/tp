07d7ee3c064a0cb4800b6622601cadd5681297f8
edwardrl101 <edward.rl101@gmail.com>
2025-03-20 18:04:16 +0800

    Remove unused import

--- src/test/java/fintrek/ExpenseManagerTest.java
+++ src/test/java/fintrek/ExpenseManagerTest.java
@@ -2,7 +2,6 @@ package fintrek;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static java.lang.Double.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
