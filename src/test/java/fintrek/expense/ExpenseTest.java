7fc36f7039cc495b47b601beba83f51bfa6012d0
edwardrl101 <edward.rl101@gmail.com>
2025-04-01 15:35:02 +0800

    Fix checkstyle errors

--- src/test/java/fintrek/expense/ExpenseTest.java
+++ src/test/java/fintrek/expense/ExpenseTest.java
@@ -62,8 +62,11 @@ class ExpenseTest {
     @Test
     public void testExpensesToStringConversion() {
         LocalDate dateToday = LocalDate.now();
-        assertEquals("eat | $10.00 | FOOD | " + dateToday ,new Expense("eat", 10.0, "food", LocalDate.now()).toString());
-        assertEquals("mrt | $2.30 | TRANSPORT | " + dateToday, new Expense("mrt", 2.30, "transport", LocalDate.now()).toString());
-        assertEquals("dinner | $15.90 | FOOD | " + dateToday,  new Expense("dinner", 15.90, "food", LocalDate.now()).toString());
+        assertEquals("eat | $10.00 | FOOD | " + dateToday,
+                new Expense("eat", 10.0, "food", LocalDate.now()).toString());
+        assertEquals("mrt | $2.30 | TRANSPORT | " + dateToday,
+                new Expense("mrt", 2.30, "transport", LocalDate.now()).toString());
+        assertEquals("dinner | $15.90 | FOOD | " + dateToday,
+                new Expense("dinner", 15.90, "food", LocalDate.now()).toString());
     }
 }
