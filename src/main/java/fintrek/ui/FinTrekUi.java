248be91f5484aea6162d042fc9fd6ad90497c1e5
Charly2312 <charlychandra99@gmail.com>
2025-04-06 01:06:55 +0800

    try to remove 'ask for input' string

--- src/main/java/fintrek/ui/FinTrekUi.java
+++ src/main/java/fintrek/ui/FinTrekUi.java
@@ -70,7 +70,7 @@ public class FinTrekUi {
      * until the exit command is given.
      */
     private void runCommandLoop() {
-        System.out.println(MessageDisplayer.ASK_FOR_INPUT);
+        //System.out.println(MessageDisplayer.ASK_FOR_INPUT);
         //System.out.print(MessageDisplayer.ARROW_FOR_INPUT);
         String userInput = reader.nextLine().trim(); // get user input
 
@@ -79,7 +79,7 @@ public class FinTrekUi {
             logger.log(Level.FINE, "User input received: " + userInput);
 
             processUserInput(userInput);
-            System.out.println(MessageDisplayer.ASK_FOR_INPUT);
+            //System.out.println(MessageDisplayer.ASK_FOR_INPUT);
             //System.out.print(MessageDisplayer.ARROW_FOR_INPUT);
             userInput = reader.nextLine().trim();
         }
